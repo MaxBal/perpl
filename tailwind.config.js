3c228ae6905d498d1ef17b4d4dc88d70c344e194
MaxBal <max.baulin15@gmail.com>
2025-06-16 23:53:50 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+      },
+      fontSize: {
+        'heading-1': '1.785rem',
+        'heading-2': '1.595rem',
+        'heading-3': '1.25rem',
+        'body': '1rem',
+        'body-sm': '0.875rem',
+        'caption': '0.813rem',
+        'micro': '0.65rem',
+      },
+      screens: {
+        mobile: {'max': '639px'},
+      },
+    },
+  },
+  plugins: [],
+};
