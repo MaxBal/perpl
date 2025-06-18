a5f059db22bdda684605221aeb27fc061e376ee9
MaxBal <max.baulin15@gmail.com>
2025-06-18 16:39:05 +0100

    Change InfoBadge color from green to orange

--- src/components/ui/InfoBadge.tsx
+++ src/components/ui/InfoBadge.tsx
@@ -1,7 +1,7 @@
 import { Info } from 'lucide-react';
 
 export const InfoBadge = () => (
-  <span className="inline-flex items-center justify-center w-4 h-4 rounded-full border border-[#00d1b3] mr-1 animate-pulse">
-    <Info className="w-[10px] h-[10px] text-[#00d1b3]" strokeWidth={1.5} />
+  <span className="inline-flex items-center justify-center w-4 h-4 rounded-full border border-[#fe5e03] mr-1 animate-pulse">
+    <Info className="w-[10px] h-[10px] text-[#fe5e03]" strokeWidth={1.5} />
   </span>
 );
