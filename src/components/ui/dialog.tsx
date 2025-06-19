761eef37a4bbee65010c1dcfe0216f4298823537
MaxBal <max.baulin15@gmail.com>
2025-06-19 19:22:40 +0100

    Add missing DialogFooter export to dialog component

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -27,3 +27,18 @@ export const DialogContent = React.forwardRef<
   </DialogPrimitive.Portal>
 ));
 DialogContent.displayName = "DialogContent";
+
+export const DialogFooter = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn(
+      "flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2",
+      className
+    )}
+    {...props}
+  />
+));
+DialogFooter.displayName = "DialogFooter";
